e031b2493a9a2559e6a2bc30cc02a182047824af
2002km <ganesanmanisha16@gmail.com>
2025-05-25 11:43:51 +0530

    created the resume website

---
+++ portfolio/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Manisha G | Resume Portfolio</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,15 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Manisha G</h1>
+      <p className="contact-info">
+      Email: <a href="mailto:ganesanmanisha16@gmail.com">ganesanmanisha16@gmail.com</a> | GitHub: <a href="https://github.com/manishag2" target="_blank">manishag2</a> | LinkedIn: <a href="https://www.linkedin.com/in/manishag2/" target="_blank">manishag2</a>
+      </p>
+    </header>
+  );
+}
+
+export default Header;
